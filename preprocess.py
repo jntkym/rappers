b665b49e34e65673fff3b6e4d00dec1bea444184
gemoku <linbaz19@yahoo.com>
2015-12-11 17:57:50 +0900

    changes in the preprocessing file

--- preprocess.py
+++ preprocess.py
@@ -10,12 +10,16 @@ import subprocess
 import codecs
 from utils import *
 
+
 reload(sys)
 sys.setdefaultencoding('utf8')
 logger = logging.getLogger(__name__)
 
+# pattern for removing redundunt spaces
 space_pat=re.compile(u'\s\s+', re.U)
-eng_words_pat = re.compile(u'[A-Za-z]*',re.U)
+
+# # pattern for removing English - not used now
+# eng_words_pat = re.compile(u'[A-Za-z]*',re.U)
 
 # aux 1.1+
 def translate_non_alphanumerics(to_translate, translate_to=None):
@@ -25,7 +29,11 @@ def translate_non_alphanumerics(to_translate, translate_to=None):
 
 #aux 1+
 def clean_lyrics(lyrics_file):
-
+    """
+    Take crawled data and do some simple cleaning on it
+    :param lyrics_file: file from Otani san - crawled data
+    :return: cleaned data, which will be fed to Kytea
+    """
     data_corpus = []
     with open(lyrics_file) as csvfile:
         reader = csv.reader(csvfile, delimiter="\t")
@@ -43,19 +51,25 @@ def clean_lyrics(lyrics_file):
                 # sentence.append(u".")
                 sentence += u'.'
 
-                # if len(sentence) > 2:
-                #     line = u"".join(sentence)
                 data_corpus.append(sentence)
 
     logger.info(" Done cleaning crawled data! ")
 
+    # saving the corpus
     with codecs.open("cleaned_lyrics.txt","w",'UTF-8') as f:
         f.write("\n".join(data_corpus))
 
-    # return "\n".join(data_corpus)
 
-# main
+# main func
 def create_corpus(crawled_lyrics_file,save=True):
+    """
+    Load cleaned crawled corpus from local folder, feed to Kytea, get the output.
+    Then laod the output and do final post-processing.
+
+    :param crawled_lyrics_file:
+    :param save:
+    :return: clean_corpus.txt file which is fed to LSTM
+    """
 
     # generating cleaned lyrics corpus from crawled data
     clean_lyrics(crawled_lyrics_file) # the corpus is one sequence of characters per line
@@ -71,7 +85,7 @@ def create_corpus(crawled_lyrics_file,save=True):
             line=line.decode(encoding="utf-8").strip()
             unk_pat.sub(u"",line)
             slash_pat.sub(u"",line)
-            triplets=line.split(u" ")
+            triplets=line.split(u" ") # take a look at Kytea output: https://github.com/chezou/Mykytea-python
             seq = []
             for item in triplets:
                 try:
