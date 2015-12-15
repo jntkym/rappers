fa06680a07dc465b7c6d590581d7b5ebc98f6b73
Leslie <leslie071564@gmail.com>
2015-12-16 01:16:06 +0900

    implement word embedding

--- WordEmbedding.py
+++ WordEmbedding.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-import tensorflow as tf
 import numpy as np
 import codecs
-#import yaml
 import sys
+ALPHA = 0.2
 
 sys.stdin  = codecs.getreader('UTF-8')(sys.stdin)
 sys.stdout = codecs.getwriter('UTF-8')(sys.stdout)
@@ -12,19 +11,30 @@ sys.stderr = codecs.getwriter('UTF-8')(sys.stderr)
 # http://www.asahi-net.or.jp/~ax2s-kmtn/ref/unicode/u3040.html 
 
 class WordEmbedding:
-    CHARACTERS = u"ぐだばむゐぁけちぱめゑあげぢひもをぃこっびゃんいごつぴやゔぅさづふゅゕうざてぶゆゖぇしでぷょえじとへよぉすどべら゙゙゙゙おずなぺりかせにほるがぜぬぼれきそねぽろぎぞのまゎくたはみわゟ"
+    CHARACTERS = u"ぐだばむゐぁけちぱめゑあげぢひもをぃこっびゃんいごつぴやゔぅさづふゅゕうざてぶゆゖぇしでぷょえじとへよぉすどべら゙゙゙゙おずなぺりかせにほるがぜぬぼれきそねぽろぎぞのまゎくたはみわゟＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ"
     def __init__(self, word):
         self.word = word
-        self._getWordEmbedding()
+        self.embedding = dict.fromkeys(list(self.CHARACTERS), 0)
+        self.inv_embedding = dict.fromkeys(list(self.CHARACTERS), 0)
+        self.frequency = dict.fromkeys(list(self.CHARACTERS), 0)
+        self._getWordEmbeddings()
+        self.vector = np.array(self.embedding.values() + self.inv_embedding.values() + self.frequency.values())
 
-    def _getWordEmbedding(self):
-        embedding = dict.fromkeys(list(CHARACTERS), 0)
+    def _getWordEmbeddings(self):
         word_list = list(self.word)
-        for i in range(len(word_list)):
-            w = word_list[i]
+        Z = len(word_list)
+        for position in range(len(word_list)):
+            w = word_list[position]
+            self.frequency[w] += 1
+
             index = self.CHARACTERS.index(w)
-            
-        
+            position_weight = ((1 - ALPHA) ** position) / Z
+            self.embedding[w] += position_weight
+            inv_position_weight = ((1 - ALPHA) ** (Z - position - 1)) / Z
+            self.inv_embedding[w] += inv_position_weight
+
 
 if __name__ == "__main__":
     hello = WordEmbedding(u"おはよう")
+    #print hello.vector
+
