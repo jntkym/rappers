5565d9a786e23136ac7c86649a2f3c3f23821e8a
Otsuki Hitoshi <hitochan777@gmail.com>
2015-12-17 09:00:42 +0900

    Modify

--- NeuralNetworkLanguageModel.py
+++ NeuralNetworkLanguageModel.py
@@ -253,21 +253,4 @@ class NeuralNetworkLanguageModel:
             return result  
 
     def next_batch(self, batch_size):
-        """Return the next `batch_size` examples from this data set."""
-        start = self._index_in_epoch
-        self._index_in_epoch += batch_size
-        if self._index_in_epoch > self._num_examples:
-            # Finished epoch
-            self._epochs_completed += 1
-            # Shuffle the data
-            perm = numpy.arange(self._num_examples)
-            numpy.random.shuffle(perm)
-            self._images = self._images[perm]
-            self._labels = self._labels[perm]
-            # Start next epoch
-            start = 0
-            self._index_in_epoch = batch_size
-            assert batch_size <= self._num_examples
-        end = self._index_in_epoch
-        return self._images[start:end], self._labels[start:end]
-
+        raise NotImplemented
