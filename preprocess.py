7cf785bfa2e38ff9b12c9db964744f3d3a60a9cb
gemoku <linbaz19@yahoo.com>
2015-12-17 12:04:45 +0900

    Hiragana corpus for Otsuki san, bigger corpus for Terada san

--- preprocess.py
+++ preprocess.py
@@ -4,17 +4,15 @@ import re
 import csv
 import time
 import sys
-import logging
-import sys
 import subprocess
 import codecs
 from utils import *
 import unicodedata
 import argparse
+import cPickle as pickle
 
 reload(sys)
 sys.setdefaultencoding('utf8')
-logger = logging.getLogger(__name__)
 
 # pattern for removing redundunt spaces
 space_pat = re.compile(u'\s\s+', re.U)
@@ -51,8 +49,9 @@ def clean_lyrics(lyrics_file):
 
                 if len(sentence) > 1:
                     data_corpus.append(sentence)
+            data_corpus.append(u"\n")
 
-    logger.info(" Done cleaning crawled data! ")
+    print(" Done cleaning crawled data! ")
 
     # saving the corpus
     with codecs.open("data/cleaned_lyrics.txt", "w", 'UTF-8') as f:
@@ -73,7 +72,7 @@ def create_corpus(crawled_lyrics_file, save=False):
     # generating cleaned lyrics corpus from crawled data
     clean_lyrics(crawled_lyrics_file)  # the corpus is one sequence of characters per line
     subprocess.call('kytea < ./data/cleaned_lyrics.txt > ./data/kytea_out.txt', shell=True)  # processing with kytea
-    logger.info(" Done kytea processing! ")
+    print(" Done kytea processing! ")
 
     pron = []
     unk_pat = re.compile(u"/補助記号/UNK")
@@ -81,7 +80,11 @@ def create_corpus(crawled_lyrics_file, save=False):
 
     with codecs.open("data/kytea_out.txt", 'UTF-8') as f:
         for line in f:
-            line = line.decode(encoding="utf-8").strip()
+            line = line.decode(encoding="utf-8")
+            if line[0] == "\n":
+                pron.append(u"\n")
+
+            line = line.strip()
             line = unk_pat.sub(u"", line)
             line = slash_pat.sub(u"", line)
 
@@ -100,41 +103,44 @@ def create_corpus(crawled_lyrics_file, save=False):
             candidate_line = re.sub(u"\s+", u"", candidate_line)
             candidate_line = re.sub(u"\d+", u"5", candidate_line)
 
-            if len(candidate_line) > 10:
+            if len(candidate_line) > 2:
                 pron.append(candidate_line)
 
-
-    NN_input = u"\n".join(pron)
-    return NN_input
+    juman_input = u"\n".join(pron)
+    juman_input = re.sub(u"\n{4}",u"\n\n",juman_input)
+    return juman_input
 
 
 # main function - creates input for the NN
-def clean_corpus(crawled_lyrics_file, model="keras", savepath=None):
+def clean_corpus(crawled_lyrics_file, savepath=None):
 
-    logger.info("Preparing data ...")
+    print("Preparing data ...")
     text = create_corpus(crawled_lyrics_file, save=False).lower()
 
     if savepath != None:
         with open(savepath, "w") as f:
             f.write(text)
-            logger.info(" Corpus saved into ----->%s " % (savepath))
+            print(" Clean data saved into ----->%s " % (savepath))
 
 def process_juman_output(juman_outfile):
 
-    logger.info("Processing juman output ...")
+    print(" Processing juman output ...")
     corpus = []
+    hiragana_corpus = []
     daihyou_vocab = {}
 
     with open(juman_outfile) as csvfile:
         reader = csv.reader(csvfile, delimiter=str(u" "))
         sent = []
-
+        hirag_sent = []
         for line in reader:
             if line[0] == u"@":
                 continue
 
             if line[0] == u"EOS":
                 corpus.append(u" ".join(sent))
+                hiragana_corpus.append(u" ".join(hirag_sent))
+                hirag_sent = []
                 sent=[]
                 continue
 
@@ -145,25 +151,34 @@ def process_juman_output(juman_outfile):
             else:
                 value = line[0]
 
+            hiragana = line[1]
+            hirag_sent.append(hiragana)
+
             key = line[0]
             daihyou_vocab[key] = value
             sent.append(key)
+
         corpus = u"\n".join(corpus)
+        hiragana_corpus = u"\n".join(hiragana_corpus)
 
-
-    print "All in all unique lemmas: %d" %(len(daihyou_vocab.values()))
+    print " All in all unique lemmas: %d" %(len(daihyou_vocab.values()))
 
     # save a txt corpus file
     with open("data/string_corpus.txt","w") as f:
         for line in corpus.split(u"\n"):
             print >> f, line
 
+    # save hiragana corpus
+    with open("data/hiragana_corpus.txt","w") as fo:
+        for line in hiragana_corpus.split(u"\n"):
+            print >> fo, line
+
     # save a vocabulary
     with open("data/daihyou_vocab.p", "w") as vocabfile:
         pickle.dump(daihyou_vocab, vocabfile)
 
     print "cleaning datadir ..."
-    subprocess.call('rm -f ./data/clean_corpus.txt ./data/kytea_out.txt ./data/cleaned_lyrics.txt',
+    subprocess.call('rm -f ./data/juman_input.txt ./data/kytea_out.txt ./data/cleaned_lyrics.txt',
                     shell=True)
 
 
@@ -175,8 +190,8 @@ def main():
     opts = parser.parse_args()
 
     if opts.crawl:
-        clean_corpus(opts.crawl[0], savepath="data/clean_corpus.txt")
-        print "Done cleaning crawled data"
+        clean_corpus(opts.crawl[0], savepath="data/juman_input.txt")
+        print " Done cleaning crawled data"
 
     if opts.juman:
         process_juman_output(opts.juman[0])
