9e4aadb1ea7bc0a5424575b53aee56ae6f4525cb
Otsuki Hitoshi <hitochan777@gmail.com>
2015-12-18 02:21:45 +0900

    Add training data for nn5; Modify tasks.py

--- tasks.py
+++ tasks.py
@@ -4,7 +4,7 @@ from invoke import run, task
 import sys
 
 from NeuralNetworkLanguageModel import * 
-import DataSet
+import generateDataForNN5 as gen
 
 sys.stdin  = codecs.getreader('UTF-8')(sys.stdin)
 sys.stdout = codecs.getwriter('UTF-8')(sys.stdout)
@@ -24,11 +24,7 @@ def train(modelPath, trainingDataPath):
             input, label = line.split("\t") 
             trainingData.append(input)
             labels.append(int(label))
-    # labels = [0,1,0,0]
-    # trainingData = [u"あこんにちは:わたし:の:なまえ:は:にぼじろう:です||こんにちは:わたし:の:なまえ:は:にぼじろう:です||こんにちは:わたし:の:なまえ:は:にぼじろう:です||あなた:が:かの:ゆうめいな:にぼじろう:さん:ですか",
-    #                 u"ちは:わ:の:まえ:は:にろう:です||にちは:たし:の:まえ:は:にう:です||ちは:わたし:の:なまえ:は:うひゃひゃ:うひひ||ほげげ:が:かの:めいな:にぼじろう:さん:でか",
-    #                 u"ちは:わ:の:まえ:は:にろう:です||にちは:たし:の:まえ:は:にう:です||ちは:わたし:の:なまえ:は:うひゃひゃ:うひひ||ほげげ:が:かの:めいな:にぼじろう:さん:で",
-    #                 u"ちは:わ:の:まえ:は:にろう:です||にちは:たし:の:まえ:は:にう:です||ちは:わたし:の:なまえ:は:うひゃひゃ:うひひ||ほげげ:が:かの:めいな:にぼじろう:さん:で"]
+
     labels = np.array(labels)
     lm = NeuralNetworkLanguageModel()
     lm.train(trainingData, labels, savePath = modelPath)
@@ -43,13 +39,9 @@ def test(modelPath, testDataPath):
             input, label = line.split("\t") 
             testData.append(input)
 
-    # testData = [u"あこんにちは:わたし:の:なまえ:は:にぼじろう:です||こんにちは:わたし:の:なまえ:は:にぼじろう:です||こんにちは:わたし:の:なまえ:は:にぼじろう:です||あなた:が:かの:ゆうめいな:にぼじろう:さん:ですか",
-    #                 u"ちは:わ:の:まえ:は:にろう:です||にちは:たし:の:まえ:は:にう:です||ちは:わたし:の:なまえ:は:うひゃひゃ:うひひ||ほげげ:が:かの:めいな:にぼじろう:さん:でか",
-    #                 u"ちは:わ:の:まえ:は:にろう:です||にちは:たし:の:まえ:は:にう:です||ちは:わたし:の:なまえ:は:うひゃひゃ:うひひ||ほげげ:が:かの:めいな:にぼじろう:さん:で",
-    #                 u"ちは:わ:の:まえ:は:にろう:です||にちは:たし:の:まえ:は:にう:です||ちは:わたし:の:なまえ:は:うひゃひゃ:うひひ||ほげげ:が:かの:めいな:にぼじろう:さん:で"]
     lm = NeuralNetworkLanguageModel()
     print lm.predict(testData, modelPath)
 
 @task
 def createData(inputFilename, outputFilename):
-    DataSet.createData(inputFilename, outputFilename)
+    gen.createData(inputFilename, outputFilename)
