ee87b0b2e5483f78e78c7933ab46bf52c3cc8daa
Otsuki Hitoshi <hitochan777@gmail.com>
2015-12-17 09:00:42 +0900

    Modify

--- tasks.py
+++ tasks.py
@@ -1,19 +1,35 @@
 # -*- coding: utf-8 -*-
 from invoke import run, task 
+# import codecs
+import sys
+
 from NeuralNetworkLanguageModel import * 
 import DataSet
+
+sys.stdin  = codecs.getreader('UTF-8')(sys.stdin)
+sys.stdout = codecs.getwriter('UTF-8')(sys.stdout)
+sys.stderr = codecs.getwriter('UTF-8')(sys.stderr)
 
 @task
 def clean():
     run("rm *.pyc")
 
 @task
-def train(modelPath):
+def train(modelPath, trainingDataPath):
+    # trainingData = []
+    # labels = []
+    # with codecs.open(trainingDataPath, "r", "UTF-8") as f:
+    #     for line in f:
+    #         line = line.strip()
+    #         input, label = line.split("\t") 
+    #         trainingData.append(input)
+    #         labels.append(int(label))
+    labels = [0,1,0,0]
     trainingData = [u"あこんにちは:わたし:の:なまえ:は:にぼじろう:です||こんにちは:わたし:の:なまえ:は:にぼじろう:です||こんにちは:わたし:の:なまえ:は:にぼじろう:です||あなた:が:かの:ゆうめいな:にぼじろう:さん:ですか",
                     u"ちは:わ:の:まえ:は:にろう:です||にちは:たし:の:まえ:は:にう:です||ちは:わたし:の:なまえ:は:うひゃひゃ:うひひ||ほげげ:が:かの:めいな:にぼじろう:さん:でか",
                     u"ちは:わ:の:まえ:は:にろう:です||にちは:たし:の:まえ:は:にう:です||ちは:わたし:の:なまえ:は:うひゃひゃ:うひひ||ほげげ:が:かの:めいな:にぼじろう:さん:で",
                     u"ちは:わ:の:まえ:は:にろう:です||にちは:たし:の:まえ:は:にう:です||ちは:わたし:の:なまえ:は:うひゃひゃ:うひひ||ほげげ:が:かの:めいな:にぼじろう:さん:で"]
-    labels = np.array([1,1,1,1])
+    labels = np.array(labels)
     lm = NeuralNetworkLanguageModel()
     lm.train(trainingData, labels, savePath = modelPath)
 
