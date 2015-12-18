ff38604378ac789de6fa59fb1cc7d729f3d99c3e
小山純汰 <junta@h122.59.229.10.1016763.vlan.kuins.net>
2015-12-18 16:45:13 +0900

    separete caller.js

--- js/caller.js
+++ js/caller.js
@@ -1,9 +1,8 @@
 function get_lyrics(){
   document.getElementById('lyrics').value = '';
-
   url = 'http://lotus.kuee.kyoto-u.ac.jp/~otani/hacku15/api/get_lyric.py';
   $.get(url, function(data){
-    var content = data.responseText
-    document.getElementById('lyrics').value = $("#text").text(content); 
+    var content = data.responseText;
+    document.getElementById('lyrics').value = content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'');
   });
-}
+};
