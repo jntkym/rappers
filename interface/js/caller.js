895e0b729bfeea8d54d0f7eda3d4ff969eea2609
小山純汰 <junta@mms.local>
2015-12-19 00:10:25 +0900

    add seed-setting function

--- interface/js/caller.js
+++ interface/js/caller.js
@@ -1,7 +1,10 @@
 function get_lyrics(){
+  var your_seed = document.getElementById('seed').value;
   document.getElementById('lyrics').value = '';
   url = 'http://lotus.kuee.kyoto-u.ac.jp/~otani/hacku15/api/get_lyric.py';
-  $.get(url, function(data){
+  $.get(url, 
+  { seed: your_seed},
+  function(data){
     var content = data.responseText;
     document.getElementById('lyrics').value = content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'');
   });
