78f8f465c2471a4b98761becff496537a353356b
小山純汰 <junta@h172.249.232.10.1017422.vlan.kuins.net>
2015-12-18 16:17:11 +0900

    add js directory

---
+++ js/caller.js
@@ -0,0 +1,9 @@
+function get_lyrics(){
+  document.getElementById('lyrics').value = '';
+
+  url = 'http://lotus.kuee.kyoto-u.ac.jp/~otani/hacku15/api/get_lyric.py';
+  $.get(url, function(data){
+    var content = data.responseText
+    document.getElementById('lyrics').value = $("#text").text(content); 
+  });
+}
